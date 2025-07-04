f375aaab88ddbc39324a714be73ed9b371abb8a0
Gwen <lgwen4084@gmail.com>
2025-07-04 15:18:20 -0600

    cron.js fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,24 @@ dotenv.config();
 
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 const app = express();
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
-app.use(rateLimiter); // Apply rate limiting middleware,
+app.use(rateLimiter); 
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
 app.use("/api/transactions", transactionsRoute);
-// app.use("/api/products", productsRoute); // Assuming you have a productsRoute
+// app.use("/api/products", productsRoute); 
 
 initDB().then(() => {
     app.listen(PORT, () => {
